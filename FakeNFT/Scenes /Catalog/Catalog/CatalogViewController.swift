6d8fe0a9ec05fff7bddff22d22c89d54dd00098c
ArtemDubovitskiy <artemdubovicki@yandex.ru>
2024-01-21 19:34:35 +0300

    Добавлено: - Ячейка таблицы с NFT - Файл декомпозици эпика Каталог

---
+++ FakeNFT/Scenes /Catalog/Catalog/CatalogViewController.swift
@@ -0,0 +1,11 @@
+//
+//  CatalogViewController.swift
+//  FakeNFT
+//
+//  Created by Artem Dubovitsky on 21.01.2024.
+//
+import UIKit
+
+final class CatalogViewController: UIViewController {
+    
+}
